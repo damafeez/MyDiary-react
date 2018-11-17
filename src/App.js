4bc4261fa8457e68269fa7a8df288d66e7b707ba
damafeez <damafeez@gmail.com>
2018-11-17 14:22:37 +0100

    chore(router): setup react-router
    - install dependencies
    - replace App.js test with shallow enzyme test
    
    finishes #162030996

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit
-            {' '}
-            <code>src/App.js</code>
-            {' '}
-and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const Home = () => <h2>HOme</h2>;
+const Login = () => <h2>Login</h2>;
+const Profile = () => <h2>Profile</h2>;
+
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/profile" component={Profile} />
+    </div>
+  </Router>
+);
 
 export default App;
