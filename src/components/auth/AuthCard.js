bc262a63c9f6dd58517c77d10945195de9304149
damafeez <damafeez@gmail.com>
2018-11-18 14:20:13 +0100

    feature(auth): complete ui
    - complete auth ui
    - add hide/reveal for signup/login
    
    working-on #162031147

--- src/components/auth/AuthCard.js
+++ src/components/auth/AuthCard.js
@@ -7,6 +7,10 @@ export default class AuthCard extends Component {
     activeView: 'login',
     'login-username': '',
     'login-password': '',
+    'signup-username': '',
+    'signup-email': '',
+    'signup-password': '',
+    'signup-fullname': '',
   };
 
   handleChange = (e) => this.setState({ [e.target.name]: e.target.value })
@@ -14,54 +18,38 @@ export default class AuthCard extends Component {
   toggleView = (view) => this.setState({ activeView: view });
 
   render() {
-    const { 'login-username': loginUsername, 'login-password': loginPassword } = this.state;
+    const {
+      'login-username': loginUsername,
+      'login-password': loginPassword,
+      'signup-username': signupUsername,
+      'signup-email': signupEmail,
+      'signup-password': signupPassword,
+      'signup-fullname': signupFullName,
+      activeView,
+    } = this.state;
     return (
       <div className="auth-card">
         <section className="login-signup">
-          <div className="login-signup_login">
+          <div className={`login-signup_login ${activeView === 'login' ? 'active' : ''}`}>
             <h2>LOGIN</h2>
             <form name="login-signup_login_form" onSubmit="login(event)">
               <InputBox handleChange={this.handleChange} value={loginUsername} label="Username" icon="icon-user-check" name="login-username" required />
               <InputBox handleChange={this.handleChange} value={loginPassword} label="Password" icon="icon-unlock" name="login-password" required />
-              <button type="button" name="submit" className="round btn">Login</button>
+              <button type="submit" name="submit" className="round btn">Login</button>
             </form>
             <p>New user? <span className="link" onClick={() => this.toggleView('signup')}>Sign up here</span></p>
           </div>
-          {/* <div className="signup">
+          <div className={`login-signup_signup ${activeView === 'signup' ? 'active' : ''}`}>
             <h2>SIGN UP</h2>
             <form onSubmit="signup(event)" name="signupForm">
-              <div className="input">
-                <svg className="icon">
-                  <use xlinkHref="svg/icons.svg#icon-user" />
-                </svg>
-                <input required name="fullName" id="signUp-fullName" type="text" />
-                <label htmlFor="signUp-fullName">Full Name</label>
-              </div>
-              <div className="input">
-                <svg className="icon">
-                  <use xlinkHref="svg/icons.svg#icon-user-check" />
-                </svg>
-                <input required name="username" id="signUp-username" type="text" />
-                <label htmlFor="signUp-username">Username</label>
-              </div>
-              <div className="input">
-                <svg className="icon">
-                  <use xlinkHref="svg/icons.svg#icon-at-sign" />
-                </svg>
-                <input required name="email" id="signUp-email" type="email" />
-                <label htmlFor="signUp-email">Email</label>
-              </div>
-              <div className="input">
-                <svg className="icon">
-                  <use xlinkHref="svg/icons.svg#icon-unlock" />
-                </svg>
-                <input required name="password" id="signUp-password" type="password" />
-                <label htmlFor="signUp-password">Password</label>
-              </div>
-              <button name="submit" className="round btn">Sign up</button>
+              <InputBox handleChange={this.handleChange} value={signupFullName} label="Full Name" icon="icon-user" name="signup-fullname" required />
+              <InputBox handleChange={this.handleChange} value={signupUsername} label="Username" icon="icon-user-check" name="signup-username" required />
+              <InputBox handleChange={this.handleChange} value={signupEmail} label="Email" icon="icon-at-sign" name="signup-email" required />
+              <InputBox handleChange={this.handleChange} value={signupPassword} label="Password" icon="icon-unlock" name="signup-email" required />
+              <button type="submit" name="submit" className="round btn">Sign up</button>
             </form>
-            <p>Existing user? <span className="link" onClick="toggleView('login')">Login here</span></p>
-          </div> */}
+            <p>Existing user? <span className="link" onClick={() => this.toggleView('login')}>Login here</span></p>
+          </div>
         </section>
         <section className="intro">
           <h1>DAILY AWESOMNESS ON THE GO.<br /> SIMPLE &amp; FAST</h1>
