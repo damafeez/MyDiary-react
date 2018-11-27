9c678612620ee532f6f60a193fad1bffb0e7687c
damafeez <damafeez@gmail.com>
2018-11-27 14:13:07 +0100

    chore(redux) complete redux setup
    - setup redux
    - add auth actions
    - add auth reducers
    
    finishes #162235475

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import store from './store';
 import Authentication from './views/Authentication';
 import Home from './views/Home';
 import './App.scss';
@@ -7,14 +9,16 @@ import './App.scss';
 const Profile = () => <h2>Profile</h2>;
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Authentication} />
-      <Route exact path="/signup" component={Authentication} />
-      <Route exact path="/profile" component={Profile} />
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Authentication} />
+        <Route exact path="/signup" component={Authentication} />
+        <Route exact path="/profile" component={Profile} />
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
