d306a3a646a6f1f34c5567cfa022c65a18c0a505
damafeez <damafeez@gmail.com>
2018-12-05 16:17:29 +0100

    chore(rename src) rename src to client
    working-on #162441635

--- config/setupTests.js
+++ config/setupTests.js
@@ -2,7 +2,7 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import thunk from 'redux-thunk';
 import configureMockStore from 'redux-mock-store';
-import API from '../src/mock/API';
+import API from '../client/mock/API';
 
 const middlewares = [thunk.withExtraArgument(API)];
 const mockStore = configureMockStore(middlewares);
